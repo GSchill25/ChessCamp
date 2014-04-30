baafafc9f0caa36f36ebddaf0b866ef43d9d6648
Graham Schilling <gschilli@andrew.cmu.edu>
2014-04-30 01:53:51 -0400

    adjusting all views to pass cuke tests

--- app/controllers/families_controller.rb
+++ app/controllers/families_controller.rb
@@ -22,7 +22,7 @@ class FamiliesController < ApplicationController
   def create
     @family = Family.new(family_params)
     if @family.save
-      redirect_to @family, notice: "#{@family.family_name} was added to the system."
+      redirect_to @family, notice: "#{@family.family_name} family was added to the system."
     else
       render action: 'new'
     end
@@ -30,7 +30,7 @@ class FamiliesController < ApplicationController
 
   def update
     if @family.update(family_params)
-      redirect_to @family, notice: "#{@family.family_name} was revised in the system."
+      redirect_to @family, notice: "#{@family.family_name} family was revised in the system."
     else
       render action: 'edit'
     end
